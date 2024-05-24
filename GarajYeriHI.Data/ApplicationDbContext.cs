4b485bcc38fe6edb075e5c203dafda12f01d26dc
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 18:39:54 +0300

    Moodel guncellemesi yapıldı

--- GarajYeriHI.Data/ApplicationDbContext.cs
+++ GarajYeriHI.Data/ApplicationDbContext.cs
@@ -29,5 +29,11 @@ namespace GarajYeriHI.Data
         public virtual DbSet<VehicleProcessType> VehicleProcessTypes { get; set;}
         public virtual DbSet<VehicleType> VehicleTypes { get; set; }
 
+        //FLUENT API
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+          //  modelBuilder.Entity<Vehicle>().Property(p => p.Name).HasMaxLength(50);
+        }
+
     }
 }
--- GarajYeriHI.Models/Vehicle.cs
+++ GarajYeriHI.Models/Vehicle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace GarajYeriHI.Models
     public class Vehicle:BaseModel
     {
         public string LicensePlate { get; set; }//plaka
+
         public string Name { get; set; }
         public double Odometer {  get; set; }
         public int VehicleTypeId { get; set; }
