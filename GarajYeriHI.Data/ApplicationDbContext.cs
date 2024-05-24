07fe57cadab2449d9427d6bd75b6aa5d0a282d45
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 18:19:04 +0300

    DbContext olusturuldu

---
+++ GarajYeriHI.Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using GarajYeriHI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeriHI.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<AppUser> Users { get; set; }
+        public virtual DbSet<Policy> Policies { get; set; }
+        public virtual DbSet<PolicyType> PolicyTypes { get; set; }
+        public virtual DbSet<Vehicle> Vehicles { get; set; }
+        public virtual DbSet<VehicleInspection> VehicleInspections { get; set;}
+        public virtual DbSet<VehiclePhoto> VehiclePhotos { get; set;}
+        public virtual DbSet<VehicleProcess> VehicleProcesses { get; set; }
+        public virtual DbSet<VehicleProcessType> VehicleProcessTypes { get; set;}
+        public virtual DbSet<VehicleType> VehicleTypes { get; set; }
+
+    }
+}
