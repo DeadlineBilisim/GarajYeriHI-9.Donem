0714ab73ad5bd41a1819df0f090c1ae31e8a181b
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 16:45:48 +0300

    AppUser oluşturuldu

---
+++ GarajYeriHI.Models/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeriHI.Models
+{
+    public class AppUser:BaseModel
+    {
+        public string FullName {  get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
