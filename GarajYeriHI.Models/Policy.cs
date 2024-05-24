532a913d40d5d1a59700075a48da71ee36d8cb3b
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 17:19:21 +0300

    Policy modeli eklendi

---
+++ GarajYeriHI.Models/Policy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeriHI.Models
+{
+    public class Policy
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; }//sigorta firması
+        public DateTime Validity { get; set; }//geçerlilik tarihi
+
+    }
+}
