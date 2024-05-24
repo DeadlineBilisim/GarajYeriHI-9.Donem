9820314ffe3932b0fc1a4de33ff66ba00d2081a8
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 18:08:50 +0300

    VehicleInspection modeli olusturuldu

---
+++ GarajYeriHI.Models/VehicleInspection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeriHI.Models
+{
+    public class VehicleInspection:BaseModel
+    {
+        public string? Description {  get; set; }
+        public double Odometer { get; set; }
+        public DateTime Validity { get; set; }
+        public string? FilePath { get; set; }//rapor dosyası tutulacak
+        public bool IsPassed { get; set; } = true;
+    }
+}
