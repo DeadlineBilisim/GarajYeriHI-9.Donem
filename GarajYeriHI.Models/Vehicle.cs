d64d551559c77087a17ce64f99a22bfff9cc5f1a
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 16:48:09 +0300

    Vehicle modeli oluşturuldu

---
+++ GarajYeriHI.Models/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeriHI.Models
+{
+    public class Vehicle:BaseModel
+    {
+        public string LicensePlate { get; set; }//plaka
+        public string Name { get; set; }
+        public double Odometer {  get; set; }
+    }
+}
