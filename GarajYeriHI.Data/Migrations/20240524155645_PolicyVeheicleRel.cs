27b8ec2046aeded64c22decb1db26742e1e95330
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 19:04:52 +0300

    Migratio yapıldı

---
+++ GarajYeriHI.Data/Migrations/20240524155645_PolicyVeheicleRel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GarajYeriHI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PolicyVeheicleRel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "VehicleId",
+                table: "Policies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Policies_VehicleId",
+                table: "Policies",
+                column: "VehicleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Policies_Vehicles_VehicleId",
+                table: "Policies",
+                column: "VehicleId",
+                principalTable: "Vehicles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Policies_Vehicles_VehicleId",
+                table: "Policies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Policies_VehicleId",
+                table: "Policies");
+
+            migrationBuilder.DropColumn(
+                name: "VehicleId",
+                table: "Policies");
+        }
+    }
+}
