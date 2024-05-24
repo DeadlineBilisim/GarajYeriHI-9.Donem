969c2fdfcd499e5fb0d5238289ab2bfbb8d9f1f1
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 18:39:44 +0300

    Program.cs ayarları yapıldı

--- GarajYeriHI.Web/Program.cs
+++ GarajYeriHI.Web/Program.cs
@@ -1,8 +1,12 @@
+using GarajYeriHI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer("slkjdsaldjasd"));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
