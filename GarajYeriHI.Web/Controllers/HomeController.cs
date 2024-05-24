50ca62e68ed9af1b17a351a3701b46fea146674e
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 19:39:11 +0300

    UserController eklendi login işlemleri yapıldı

--- GarajYeriHI.Web/Controllers/HomeController.cs
+++ GarajYeriHI.Web/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using GarajYeriHI.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace GarajYeriHI.Web.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
---
+++ GarajYeriHI.Web/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using GarajYeriHI.Data;
+using GarajYeriHI.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace GarajYeriHI.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AppUser appUser)
+        {
+            AppUser user = _context.Users.FirstOrDefault(u => u.UserName == appUser.UserName && u.Password == appUser.Password);
+            if(user!=null)
+            {
+                List<Claim> claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+                claims.Add(new Claim(ClaimTypes.Name,user.UserName));
+                claims.Add(new Claim(ClaimTypes.GivenName, user.FullName));
+                claims.Add(new Claim(ClaimTypes.Role, user.IsAdmin ? "Admin" : "User"));
+                ClaimsIdentity identity = new ClaimsIdentity(claims,CookieAuthenticationDefaults.AuthenticationScheme);
+
+              await  HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+
+               await HttpContext.SignInAsync(new ClaimsPrincipal(identity));
+
+                return RedirectToAction("Index", "Vehicle");
+            }
+            return View();
+        }
+
+     
+    }
+}
