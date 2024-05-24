3cdb2dcc8ed08331c8eef18e4f7c9b4ce8d7f94d
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 18:09:16 +0300

    IsAdmin eklendi

--- GarajYeriHI.Models/AppUser.cs
+++ GarajYeriHI.Models/AppUser.cs
@@ -11,6 +11,7 @@ namespace GarajYeriHI.Models
         public string FullName {  get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public bool IsAdmin { get; set; } = false;
 
         public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
     }
