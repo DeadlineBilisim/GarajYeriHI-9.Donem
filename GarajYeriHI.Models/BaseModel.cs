06e7edfbab7f1e8771f23ed8c7ba88677b8a1c12
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-24 16:43:43 +0300

    BaseModel oluşturuldu.

---
+++ GarajYeriHI.Models/BaseModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeriHI.Models
+{
+    public class BaseModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public Guid Guid { get; set; } = Guid.NewGuid();
+        public bool IsDeleted { get; set; } = false;
+        public bool IsActive { get; set; } = true;
+        public DateTime DateCreated { get; set; }=DateTime.Now;
+        public DateTime? DateDeleted { get; set; }
+
+
+    }
+}
