6c693693c73e953291bd82727d29313f901a6f4d
Emre Ozcelikel <emre@ozcelikel.com>
2024-05-31 18:24:02 +0300

    backend tamamlandı

---
+++ GarajYeriHI.Web/Controllers/VehicleTypeController.cs
@@ -0,0 +1,66 @@
+﻿using GarajYeriHI.Data;
+using GarajYeriHI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarajYeriHI.Web.Controllers
+{
+    public class VehicleTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VehicleTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _context.VehicleTypes.Where(vt => !vt.IsDeleted) });
+        }
+
+        [HttpPost]
+        public IActionResult Add(VehicleType vehicleType)
+        {
+            _context.VehicleTypes.Add(vehicleType);
+            _context.SaveChanges();
+            return Ok(vehicleType);
+        }
+
+        [HttpPost]
+        public IActionResult HardDelete(VehicleType vehicleType)
+        {
+            _context.VehicleTypes.Remove(vehicleType);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult SoftDelete(int id)
+        {
+            var vehicleType = _context.VehicleTypes.Find(id);
+            vehicleType.IsDeleted = true;
+            _context.VehicleTypes.Update(vehicleType);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult Update(VehicleType vehicleType) 
+        {
+            _context.VehicleTypes.Update(vehicleType);
+            _context.SaveChanges();
+            return Ok(vehicleType);
+        }
+
+        [HttpPost]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_context.VehicleTypes.Find(id));
+        }
+    }
+}
