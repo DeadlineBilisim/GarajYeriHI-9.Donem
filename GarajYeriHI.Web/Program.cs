41ea2612f9c92e99d872b1e53a6536ca976d8c1f
Emre Ozcelikel <emre@ozcelikel.com>
2024-06-07 08:40:51 +0300

    addcsoped duzeltildi

--- GarajYeriHI.Web/Program.cs
+++ GarajYeriHI.Web/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped<IRepository<VehicleTypeController>,Repository<VehicleType>>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
 {
