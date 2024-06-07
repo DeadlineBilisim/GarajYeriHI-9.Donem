d4c72218af9dab8fd7431e58862ebff3e307a2e4
Emre Ozcelikel <emre@ozcelikel.com>
2024-06-07 16:50:16 +0300

    respository'ye gecildi

--- GarajYeriHI.Web/Controllers/VehicleProcessTypeController.cs
+++ GarajYeriHI.Web/Controllers/VehicleProcessTypeController.cs
@@ -1,5 +1,6 @@
 ﻿using GarajYeriHI.Data;
 using GarajYeriHI.Models;
+using GarajYeriHI.Repository.Shared.Abstract;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,11 +9,11 @@ namespace GarajYeriHI.Web.Controllers
     [Authorize(Roles ="Admin")]
     public class VehicleProcessTypeController : Controller
     {
-        private readonly ApplicationDbContext _context;
+      private readonly IRepository<VehicleProcessType> _repository;
 
-        public VehicleProcessTypeController(ApplicationDbContext context)
+        public VehicleProcessTypeController(IRepository<VehicleProcessType> repository)
         {
-            _context = context;
+            _repository = repository;
         }
 
         public IActionResult Index()
@@ -22,48 +23,36 @@ namespace GarajYeriHI.Web.Controllers
 
         public IActionResult GetAll()
         {
-            return Json(new { data=_context.VehicleProcessTypes.Where(vpt=>!vpt.IsDeleted).ToList() });
+            return Json(new { data=_repository.GetAll() });
         }
 
         [HttpPost]
         public IActionResult Add(VehicleProcessType vehicleProcessType)
         {
-            _context.VehicleProcessTypes.Add(vehicleProcessType);
-            _context.SaveChanges();
-            return Ok(vehicleProcessType);
+           
+            return Ok(_repository.Add(vehicleProcessType));
         }
         [HttpPost]
         public IActionResult Update(VehicleProcessType vehicleProcessType)
         {
-            _context.VehicleProcessTypes.Update(vehicleProcessType);
-            _context.SaveChanges();
-            return Ok(vehicleProcessType);
+           
+            return Ok(_repository.Update(vehicleProcessType));
         }
 
         [HttpPost]
         public IActionResult SoftDelete(int id)
         {
-            var vehicleProcessType = _context.VehicleProcessTypes.Find(id);
-            vehicleProcessType.IsDeleted = true;
-            vehicleProcessType.DateDeleted = DateTime.Now;
-            _context.VehicleProcessTypes.Update(vehicleProcessType);
-            _context.SaveChanges();
+          _repository.DeleteById(id);
             return Ok();
         }
 
-        [HttpPost]
-        public IActionResult HardDelete(VehicleProcessType vehicleProcessType) 
-        {
-            _context.VehicleProcessTypes.Remove(vehicleProcessType);
-            _context.SaveChanges();
-            return Ok();
-        }
+      
 
         [HttpPost]
         public IActionResult GetById(int id)
         {
 
-            return Ok(_context.VehicleProcessTypes.Find(id));
+            return Ok(_repository.GetById(id));
 
         }
     }
